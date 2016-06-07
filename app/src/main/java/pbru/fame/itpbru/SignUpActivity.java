80cedd65eab6351aee24f98a351b2cea82b873fd
intawad <intawad.d@gmail.com>
2016-06-07 11:02:12 +0700

    Check Space finish

--- app/src/main/java/pbru/fame/itpbru/SignUpActivity.java
+++ app/src/main/java/pbru/fame/itpbru/SignUpActivity.java
@@ -2,12 +2,58 @@ package pbru.fame.itpbru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText, surenameEditText, userEditText, passwordEditText;
+    private String nameString, surenameString, userString, passwordString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surenameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+
+
+    }//Main Method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim(); //เก็บค่าเปลี่ยนเป็น Text ลบช่องว่าง
+        surenameString = surenameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+         //check space
+        if (checkSpace()) {
+            //True
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "Error!", "Please Input Text");
+
+
+
+        } else {
+            //False
+
+        }
+
+    } //clickSign
+
+    private boolean checkSpace() {
+
+        boolean result = true;
+
+        result = nameString.equals("")||surenameString.equals("")||
+                userString.equals("")||passwordString.equals("");
+
+        return result;
     }
-}
+
+}//Main Class
